2b2ac7ae1c835626e179b182c720fc327a07e3a1
rlathf815 <rlathfrlathf@gmail.com>
2023-12-28 00:03:48 +0900

    SR

--- Assets/Scripts/SpeechRecognition.cs
+++ Assets/Scripts/SpeechRecognition.cs
@@ -1,4 +1,6 @@
 using Microsoft.CognitiveServices.Speech;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class SpeechRecognition : MonoBehaviour
@@ -8,13 +10,41 @@ public class SpeechRecognition : MonoBehaviour
 
     private async void Start()
     {
-        var config = SpeechConfig.FromSubscription(speechKey, serviceRegion);
+        await RecognizeSpeechAsync();
+    }
 
+    private async Task RecognizeSpeechAsync()
+    {
+        var config = SpeechConfig.FromSubscription(speechKey, serviceRegion);
         using (var recognizer = new SpeechRecognizer(config))
         {
-            var result = await recognizer.RecognizeOnceAsync();
+            while (true) 
+            {
+                var result = await recognizer.RecognizeOnceAsync();
 
-            Debug.Log(result.Text);
+                if (result.Reason == ResultReason.RecognizedSpeech)
+                {
+                    UnityEngine.Debug.Log($"음성인식 결과: {result.Text}");
+                }
+                else if (result.Reason == ResultReason.NoMatch)
+                {
+                    UnityEngine.Debug.Log($"인식불가");
+                }
+                else if (result.Reason == ResultReason.Canceled)
+                {
+                    var cancellation = CancellationDetails.FromResult(result);
+                    UnityEngine.Debug.Log($"취소 => {cancellation.Reason}");
+
+                    if (cancellation.Reason == CancellationReason.Error)
+                    {
+                        UnityEngine.Debug.Log($"취소: ErrorCode={cancellation.ErrorCode}");
+                        UnityEngine.Debug.Log($"취소: ErrorDetails={cancellation.ErrorDetails}");
+                        UnityEngine.Debug.Log($"취소: 구독정보 갱신");
+                    }
+                }
+            }
         }
     }
+
+    
 }
